9e49145fc8b23e627a6e763c38472038b1699375
Alex Goodman <wgoodman@arinc.com>
2016-07-01 11:30:05 -0400

    added umount option to global module methods

--- overlayUtils/__init__.py
+++ overlayUtils/__init__.py
@@ -1,15 +1,14 @@
 from .overlay import OverlayFS, OverlayFSManager, OverlayFSDoesNotExist
 
-
 def mount(directory, lower_dir, upper_dir, mount_table=None):
-    """Creates a mount"""
     return OverlayFS.mount(directory, lower_dir, upper_dir,
             mount_table=mount_table)
 
+def umount(directory):
+    return OverlayFS.umount(directory)
 
 def list(mount_table=None):
     return OverlayFSManager.list(mount_table=mount_table)
-
 
 def get(mount_point, mount_table=None):
     return OverlayFSManager.get(mount_point, mount_table=mount_table)
